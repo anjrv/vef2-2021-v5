386bc11753a3175c43fbb21f8cc88c8975aabd4a
Anna Jaerving <annajrving@gmail.com>
2021-03-23 15:47:37 +0000

    some helmet

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Helmet from "react-helmet";
 
 export function NotFound() {
   return (
     <div>
+      <Helmet title="Fannst ekki" />
       <h2>Villa kom upp!</h2>
       <p>404: Fannst ekki</p>
     </div>
